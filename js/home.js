8c66904d98da1e6b568e4fa1b8bd8393cc833980
Mariam Saikodze <116747017+kodze8@users.noreply.github.com>
2025-05-09 14:51:13 +0200

    l

--- js/home.js
+++ js/home.js
@@ -1,14 +1,20 @@
 const img = document.getElementById('img');
 const title = document.getElementById('title');
 
-img.addEventListener('click', function() {
-   
+function triggerAnimationAndRedirect() {
     img.classList.add('move-img');
     title.classList.add('move-title');
 
     setTimeout(function() {
         window.location.href = "./pages/portfolio.html";
-    }, 1500); 
+    }, 1500);
+}
 
-});
+img.addEventListener('click', triggerAnimationAndRedirect);
 
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        event.preventDefault(); 
+        triggerAnimationAndRedirect();
+    }
+})
